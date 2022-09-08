5a7da56b61e65d875c28cd043d5aeb9ec3f263ad
Bradley Wicks <ST10025342@vgconnect.edu.za>
2022-09-08 14:01:21 +0200

    Map and GameEngine classes created

---
+++ GADE6122_POE_PART1/GameEngine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6122_POE_PART1
+{
+    class GameEngine
+    {
+        private Map map; //Map object
+        private static readonly char[] symbols = { 'H', '.', 'S', 'X' }; //char array of symbols which represent the tiletype enum
+        public GameEngine() //constructor that gives values to the map object
+        {
+            map = new Map(10, 20, 10, 20, 3); //Min Width, Max Width, min Height, max Height, num of Enemies
+        }
+
+        public Map getMap()
+        {
+            return map; //returns map
+        }
+
+        public bool MovePlayer(Character.Movement m)
+
+        {
+            //x and y variables of the hero on the map
+            int x = map.getHero().getX();
+            int y = map.getHero().getY();
+
+            bool result = false;//result to return
+            if (m != Character.Movement.Stationary) //if m is not = to stationay the code below will run
+            {
+                map.getMap()[x, y] = new EmptyTile(x, y, Tile.TileType.Empty); //puts an empty tile where the hero was
+                map.getHero().Move(m); //Calls get hero which calls move
+                map.getMap()[x, y] = map.getHero(); //sets heroes new location on the map array to the hero object that was created
+                map.UpdateVision(); //updates vision
+                result = true; //sets result to true
+            }
+            return result; //returns result
+        }
+        public override string ToString()
+        {
+            //nested forloop which parses the map array and returns it in a string format
+            string result = "";
+            for (int i = 0; i < map.getMap().GetLength(0); i++)
+            {
+                for (int j = 0; j < map.getMap().GetLength(1); j++)
+                {
+                    if (map.getMap()[i, j] is EmptyTile) //For the "."
+                    {
+                        result += symbols[1] + " ";
+                    }
+                    else if (map.getMap()[i, j] is Obstacle) //For the "X"
+                    {
+                        result += symbols[3] + " ";
+                    }
+                    else if (map.getMap()[i, j] is Enemy) //For the "S"
+                    {
+                        result += symbols[2] + " ";
+                    }
+                    else if (map.getMap()[i, j] is Hero) //For the "H"
+                    {
+                        result += symbols[0] + " ";
+                    }
+
+                }
+                result += "\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ GADE6122_POE_PART1/Map.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6122_POE_PART1
+{
+    class Map
+    {
+        //Creating variables which hold the Hero and Enemy tile type
+        Tile.TileType typeH = Tile.TileType.Hero;
+        Tile.TileType typeE = Tile.TileType.Enemy;
+
+        private Tile[,] map; //2D Tile array
+        private Hero hero; //Hero object
+        private Enemy[] enemy;//1D Enemy array holding enemy objects
+        private int width; //map width
+        private int height; //map height
+        private int enemyCount = 0; //enemy counter
+        private Random randNum = new Random(); //random number object
+
+        //Constructor
+        public Map(int minW, int maxW, int minH, int maxH, int numE)
+        {
+            height = randNum.Next(minH, maxH);
+            width = randNum.Next(minW, maxW);
+            map = new Tile[height, width];
+            enemy = new Enemy[numE];
+            //Create() for hero
+            Create(typeH);
+            //Create() for Enemy array
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                Create(typeE);
+                enemyCount++;
+            }
+        }
+
+        //Accessors & Mutators
+        public Tile[,] getMap()
+        {
+            return map;
+        }
+        public void setMap(Tile[,] m)
+        {
+            map = m;
+        }
+
+        public Hero getHero()
+        {
+            return hero;
+        }
+        public void setHero(Hero h)
+        {
+            hero = h;
+        }
+
+        public Enemy[] getEnemy()
+        {
+            return enemy;
+        }
+        public void setEnemy(Enemy[] e)
+        {
+            enemy = e;
+        }
+
+        public int getWidth()
+        {
+            return width;
+        }
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+
+        public int getHeight()
+        {
+            return height;
+        }
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+
+        //Update vision of hero or enemy
+        public void UpdateVision()
+        {
+            Tile[] hVis = new Tile[4];
+            hVis[0] = map[hero.getX(), hero.getY() - 1];
+            hVis[1] = map[hero.getX(), hero.getY() + 1];
+            hVis[2] = map[hero.getX() - 1, hero.getY()];
+            hVis[3] = map[hero.getX() + 1, hero.getY()];
+            hero.setPlayerVision(hVis);
+
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                Tile[] eVis = new Tile[4];
+                eVis[0] = map[enemy[i].getX(), enemy[i].getY() - 1];
+                eVis[1] = map[enemy[i].getX(), enemy[i].getY() + 1];
+                eVis[2] = map[enemy[i].getX() - 1, enemy[i].getY()];
+                eVis[3] = map[enemy[i].getX() + 1, enemy[i].getY()];
+                enemy[i].setPlayerVision(eVis);
+            }
+
+        }
+
+        //creates hero or enemy
+        private Tile Create(Tile.TileType t)
+        {
+            Random rand = new Random();
+            int numX = rand.Next();
+            int numY = rand.Next();
+            bool valid = false;
+            Tile result = null;
+            if (t == typeH)
+            {
+
+                while (!valid)
+                {
+                    if (map[numX, numY].getType() == typeE || (map[numX, numY] is Obstacle))
+                    {
+                        numX = rand.Next();
+                        numY = rand.Next();
+                        valid = false;
+                    }
+                    else
+                    {
+                        hero.setX(numX);
+                        hero.setY(numY);
+
+                        valid = true;
+                        result = hero;
+                    }
+                }
+            }
+            else
+            {
+                valid = false;
+                while (!valid)
+                {
+
+                    if (map[numX, numY].getType() == typeH || map[numX, numY].getType() == typeE || (map[numX, numY] is Obstacle))
+                    {
+                        numX = rand.Next();
+                        numY = rand.Next();
+                        valid = false;
+                    }
+                    else
+                    {
+                        enemy[enemyCount].setX(numX);
+                        enemy[enemyCount].setY(numY);
+                        valid = true;
+                        result = enemy[enemyCount];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
