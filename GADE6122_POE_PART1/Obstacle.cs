197d03529d17377046dd5ff4742837d8ee3e09cc
Bradley Wicks <ST10025342@vgconnect.edu.za>
2022-09-08 13:46:27 +0200

    Obstacle and Enemy classes made

---
+++ GADE6122_POE_PART1/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6122_POE_PART1
+{
+    class Obstacle : Tile
+    {
+        public Obstacle(int x, int y, TileType t) : base(x, y, t)
+        {
+        }
+    }
+}
